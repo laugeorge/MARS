1514aeaf20b770d6f4936f7724f0e1a2ea377aa6
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-08-20 15:31:36 -0400

    registration boilerplate if mysql used

--- server.js
+++ server.js
@@ -24,7 +24,20 @@ app.get('/', function(req, res){
         if(err) throw err;
         var user = data;
         console.log(user);
+        // TODO: EJS? What are we rendering?
         // res.render('home', {user: user});
+    });
+});
+
+app.post('/register', function(req, res){
+    // TODO: change for information we want
+    var person = {
+        email: req.body.email
+    };
+
+    connection.query('INSERT INTO users SET ?', person, function(err,result){
+        if (err) throw err;
+        res.redirect('/'); // TODO: check redirection link
     });
 });
 
