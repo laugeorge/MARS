09147e6f51ad850e0543370763a2ba1ac7d35219
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-08-21 01:39:24 -0400

    front end rover logic completegit add -A! *crosses fingers*

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -51,11 +51,6 @@
 // curiosity rover parts
 var curiosity = ['Mast Camera', 'Chemistry and Camera complex', 'Navigation cameras', 'Antenne UHF', 'RTG', 'Rover Environmental Monitoring Station', 'Hazard avoidance cameras', 'Antenne gran gain', 'Dynamic Albedo of Neutrons', 'Radiation assessment detector', 'Sample Analysis at Mars', 'Dust Removal Tool', 'Chemistry and Mineralogy Spectrometer', 'Mars Hand Lens Imager', 'Alpha Particle X-ray Spectrometer', 'Mars Descent Imager', 'Robotic arm'];
 
-var broken = {
-    task: randRange(rover),
-    role_id: 1
-};
-
 // pick a random thing to break
 function randRange(rover) {
     var whatsBroken = rover[Math.floor(rover.length * Math.random())];
@@ -65,21 +60,14 @@ function randRange(rover) {
 function addToList() {
     var a = randRange(curiosity);
 
-    if (broken.length>0){
-        for(var i=0; i<broken.length; i++){
-            if (broken[i] != a){
-                broken.push(a);
-            }
-        }
-    } else {
-        broken.push(a);
-    }
+    var brokenThing = {
+        task: `fix Curiosity's ${a}`,
+        role_id: 1
+    };
+
+    console.log(brokenThing);
+
+    setTimeout(addToList, 5000);
 }
 
-var interval = setInterval(function(){
-    // method to be executed
-
-}, 5000);
-
-
-clearInterval(interval);
+addToList();
