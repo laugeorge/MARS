068b8141395d872f6697d71232708d4e99102539
shanekia <jshanekia@gmail.com>
2019-08-21 19:58:37 -0400

    database data

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+insert into users (first_name,last_name) values ( "Chris"," Hubbard");
+insert into users (first_name,last_name) values ( "Pooja","Chauhan");
+insert into users (first_name,last_name) values ( "Kim","Spiegel");
+insert into users (first_name,last_name) values ( "George","Lau");
+insert into users (first_name,last_name) values ( "Shanekia", "Jones");
+
