3cf7b32a65b2b74bf36203637b5dfa40ba854f4e
georgelau <laugeorge@gmail.com>
2019-08-24 11:44:57 -0400

    latest update from all my folder with todolist2 as a testground

--- todolist/animalsDB.sql
+++ todolist/animalsDB.sql
@@ -1,37 +1,23 @@
--- Drops the animals_db if it exists currently --
-DROP DATABASE IF EXISTS animals_db;
--- Creates the "animals_db" database --
-CREATE DATABASE animals_db;
+SELECT 
+    task AS 'TODO',
+     CASE
+        WHEN assigned = 0 THEN 'NOT ASSIGNED'
+         ELSE 'ASSIGNED'
+         -- ELSE CONCAT(first_name, ' ', last_name)
+     END AS 'ASSIGNMENT',
+     CASE
+        WHEN completed = 0 THEN 'NOT COMPLETED'
+        ELSE 'COMPLETED'
+     END AS 'STATUS',
+     type
+FROM todo
+    LEFT JOIN jobs
+        ON todo.role_id = jobs.role_id
+    LEFT JOIN roles
+        ON roles.id = todo.role_id
+    GROUP BY task
+    ORDER BY created_at;
+Collapse
 
--- Makes it so all of the following code will affect animals_db --
-USE animals_db;
 
--- Creates the table "people" within animals_db --
-CREATE TABLE people (
-  -- Makes a string column called "name" which cannot contain null --
-  name VARCHAR(30) NOT NULL,
-  -- Makes a boolean column called "has_pet" which cannot contain null --
-  has_pet BOOLEAN NOT NULL,
-  -- Makes a sting column called "pet_name" --
-  pet_name VARCHAR(30),
-  -- Makes an numeric column called "pet_age" --
-  pet_age INTEGER(10)
-);
 
--- Creates new rows containing data in all named columns --
-INSERT INTO people (name, has_pet, pet_name, pet_age)
-VALUES ("Ahmed", TRUE, "Rockington", 100);
-
-INSERT INTO people (name, has_pet, pet_name, pet_age)
-VALUES ("Ahmed", TRUE, "Rockington", 100);
-
-INSERT INTO people (name, has_pet, pet_name, pet_age)
-VALUES ("Jacob", TRUE, "Misty", 10);
-
-INSERT INTO people (name, has_pet)
-VALUES ("Peter", false);
-
--- Updates the row where the column name is peter --
-UPDATE people
-SET has_pet = true, pet_name = "Franklin", pet_age = 2
-WHERE name = "Peter";
