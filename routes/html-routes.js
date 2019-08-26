893e339e03b85ba955596bc7397fc5c4dbe9d0a6
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-08-26 19:49:44 -0400

    updated folder structure

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+
+module.exports = function(app) {
+
+    app.get("/tables", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/tables.html"));
+    });
+
+    app.get("/reserve", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    });
+
+    // If no matching route is found default to home
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
