ee4a4c4b4a82b17ab2aaee3d9a9221feda3a12b6
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-08-26 20:57:12 -0400

    tables and dummy data

--- db/schema.sql
+++ db/schema.sql
@@ -2,49 +2,30 @@ CREATE DATABASE mars_db;
 USE mars_db;
 
 CREATE TABLE users(
-	id INT auto_increment primary key,
-    first_name varchar(255),
-    last_name varchar(255),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
-    
--- Create table authenticate(
---  		id Int auto_increment primary key,
---         passKey varchar(255),
---         user_id int not null,
---          Foreign Key (user_id) references users(id)
---         );
---     
---   CREATE TABLE chat(
--- 			 		id Int auto_increment primary key,
---                     message varchar(255),
---                     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---                     user_id int not null,
---                     foreign key (user_id) references users(id)
---                     );
-
--- create table role(
--- 	id Int auto_increment primary key,
---     type varchar(255),
---     user_id int not null,
---     foreign key (user_id) references users(id)
---     
--- );
-
-create table todo (
-	id Int auto_increment primary key,
-		task varchar(255),
-        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		user_id int not null,
-       -- foreign key(role_id) references role(id),
-		foreign key (user_id) references users(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    username VARCHAR(255),
+    password VARCHAR(255),
+    job_title VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
-create table active (
-       todo_id int not null,
-		user_id int not null,
-        foreign key (todo_id) references todo(id),
-		foreign key (user_id) references users(id),
-        primary key (todo_id, user_id)
+
+CREATE TABLE todo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task VARCHAR(255),
+    user_id INT,
+    completed BOOLEAN DEFAULT 0,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-         
+
+
+CREATE TABLE chat (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    message VARCHAR(255),
+    user_id INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,23 @@
-insert into users (first_name,last_name) values ( "Chris"," Hubbard");
-insert into users (first_name,last_name) values ( "Pooja","Chauhan");
-insert into users (first_name,last_name) values ( "Kim","Spiegel");
-insert into users (first_name,last_name) values ( "George","Lau");
-insert into users (first_name,last_name) values ( "Shanekia", "Jones");
+-- USERS 
+INSERT INTO users(first_name, last_name, username, password, job_title, created_at) VALUES
+('Chris','Hubbard','NeoParadigm84','1234','Engineer', '2018-04-01 13:00:00'),
+('George','Lau','George_Lau','1111','Mechanic', '2018-04-01 13:00:00'),
+('Kim','Spiegel','kimSpiegel04','password','Specialist', '2018-04-01 13:00:00'),
+('Pooja','Chauhan','CCChauhan','guest','Microbiologist', '2018-04-01 13:00:00'),
+('Shanekia','Jones','S_Jones','4321','Space DJ', '2018-04-01 13:00:00');
 
+-- SOME DUMMY TODOS
+INSERT INTO todo (task, user_id) VALUES 
+('Check garden soil', 5),
+('Check air quality', 3),
+('Create bomb playlist', 6),
+('Clean filters', 2),
+('Write logs', 4);
+
+-- DUMMY CHATS
+INSERT INTO chat (message, user_id) VALUES
+('Hello and goodbye. It is a desert.', 5),
+('Mars is hot as balls', 2),
+('I keep getting dust in my underwear', 6),
+("Help I'm trapped in the airlock", 3),
+('Can someone grab a water for me from the kitchen?',4);
